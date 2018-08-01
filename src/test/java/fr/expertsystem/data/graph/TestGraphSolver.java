8e9219cbb0250bc4550da2ede468a6067f9595ad
Ourten <ourtencoop@gmail.com>
2018-08-01 15:11:23 +0200

    Add Graph unit tests

---
+++ src/test/java/fr/expertsystem/data/graph/TestGraphSolver.java
@@ -0,0 +1,81 @@
+package fr.expertsystem.data.graph;
+
+import fr.expertsystem.data.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TestGraphSolver
+{
+    @Test
+    void expandedRule()
+    {
+        GlobalState state = new GlobalState();
+        state.setFactState(new Fact("A"), FactState.TRUE);
+        state.setFactState(new Fact("B"), FactState.TRUE);
+
+        Rule rule = Rule.build().fact("A").cond(Conditions.AND)
+                .cond(Conditions.OPEN_PARENTHESIS).fact("B").cond(Conditions.OR).fact("C").cond(Conditions.CLOSE_PARENTHESIS).imply().fact("D").create();
+
+        Graph graph = new Graph();
+        RuleExpander.expandRules(Collections.singletonList(rule)).forEach(graph::addRule);
+
+        assertThat(FactSolver.query(new Fact("D"), state.copy(), graph)).isEqualTo(FactState.TRUE);
+
+        state.setFactState(new Fact("B"), FactState.FALSE);
+        assertThat(FactSolver.query(new Fact("D"), state.copy(), graph)).isEqualTo(FactState.FALSE);
+    }
+
+    @Test
+    void andConclusion()
+    {
+        GlobalState state = new GlobalState();
+        state.setFactState(new Fact("A"), FactState.TRUE);
+        state.setFactState(new Fact("B"), FactState.TRUE);
+
+        // A + B = C + D
+        Rule rule = Rule.build().fact("A").cond(Conditions.AND).fact("B").imply().fact("C").cond(Conditions.AND).fact("D").create();
+
+        Graph graph = new Graph();
+        graph.addRule(rule);
+
+        assertThat(FactSolver.query(new Fact("C"), state.copy(), graph)).isEqualTo(FactState.TRUE);
+        assertThat(FactSolver.query(new Fact("D"), state.copy(), graph)).isEqualTo(FactState.TRUE);
+    }
+
+    @Test
+    void firstDegreeCircularDep()
+    {
+        GlobalState state = new GlobalState();
+
+        Rule firstRule = Rule.build().fact("A").imply().fact("B").create();
+        Rule secondRule = Rule.build().fact("B").imply().fact("A").create();
+
+        Graph graph = new Graph();
+        graph.addRule(firstRule);
+        graph.addRule(secondRule);
+
+        assertThrows(RuntimeException.class, () -> FactSolver.query(new Fact("B"), state, graph));
+        assertThrows(RuntimeException.class, () -> FactSolver.query(new Fact("A"), state, graph));
+    }
+
+    @Test
+    void secondDegreeCircularDep()
+    {
+        GlobalState state = new GlobalState();
+        state.setFactState(new Fact("A"), FactState.TRUE);
+
+        Rule firstRule = Rule.build().fact("A").cond(Conditions.AND).fact("B").imply().fact("C").create();
+        Rule secondRule = Rule.build().fact("C").imply().fact("B").create();
+
+        Graph graph = new Graph();
+        graph.addRule(firstRule);
+        graph.addRule(secondRule);
+
+        assertThrows(RuntimeException.class, () -> FactSolver.query(new Fact("B"), state, graph));
+        assertThrows(RuntimeException.class, () -> FactSolver.query(new Fact("C"), state, graph));
+    }
+}
